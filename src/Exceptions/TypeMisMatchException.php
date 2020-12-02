a2569fe3d2972b426d6e3c24fdee589a41b9c085
Gijs Goudzwaard <gijs.goudzwaard@gmail.com>
2020-12-02 15:07:40 +0100

    Added value to the type mismatch exception for further tracing

--- src/Exceptions/TypeMisMatchException.php
+++ src/Exceptions/TypeMisMatchException.php
@@ -6,13 +6,14 @@ use Exception;
 
 class TypeMisMatchException extends Exception
 {
-    public static function create(string $field, string $expected, ?string $type = null): self
+    public static function create(string $field, string $expected, ?string $type = null, ?string $value = null): self
     {
         return new static(sprintf(
-            'Type value mismatch! Expected: %s%s on field %s',
+            'Type value mismatch! Expected: %s%s on field %s with value %s',
             $expected,
             (is_null($type) ? '' : ', got: ' . $type),
-            $field
+            $field,
+            $value
         ));
     }
 }
